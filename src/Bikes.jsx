78e6639c16f2d720cc061fde2fe83d765f9e4d73
Jhojan Martínez <jhojanmartinez04@hotmail.com>
2022-10-02 16:57:24 -0500

    Adding info about each company

--- src/Bikes.jsx
+++ src/Bikes.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Load } from "./services/stations";
 import imagenLogo from "./assets/planeta.png";
-import { Heading, Box, Image,Flex, Center, Text, Spacer, Tag, Button} from '@chakra-ui/react';
+import { Heading, Box, Image, Flex, Center, Text, Spacer, Tag, Button } from '@chakra-ui/react';
 
 export default function Bikes() {
   const [stations, setStations] = useState([]);
@@ -13,41 +13,46 @@ export default function Bikes() {
 
   return (
     <>
-    
-    
-   <Center> <Image m={4} src={imagenLogo} alt="BikeLogo" width={200}/> </Center> 
-      <Heading as="h1" size="4xl">Bikes</Heading>
+      <Center>
+        <Heading as="h1" size="4xl" p={5}>
+          <Box width={400} background="orange.200" borderRadius={20} p={10} m={5} textAlign="center">
+            Bikes
+          </Box>
+        </Heading>
+      </Center>
+      <Center><Image m={4} src={imagenLogo} alt="BikeLogo" width={300} /> </Center>
+
 
       <section>
-        
-          {stations.map((stations) => (
 
-            <Box key={stations.id} bg="yellow.100" p={10} m={5} borderRadius={20}>
-              <Flex>
-                <Text fontSize="2xl">
-                  City <strong>{stations.location.city}</strong> -
-                </Text>
-                <Tag height={45} colorScheme="red" >
-                  {stations.location.country}
-                </Tag>
-                <br />
-                <Spacer />
-                <a href={`/infoBikes/${stations.id}`}>
+        {stations.map((stations) => (
+
+          <Box key={stations.id} bg="yellow.100" p={10} m={5} borderRadius={20}>
+            <Flex>
+              <Text fontSize="2xl">
+                City <strong>{stations.location.city}</strong> -
+              </Text>
+              <Tag height={45} colorScheme="red" >
+                {stations.location.country}
+              </Tag>
+              <br />
+              <Spacer />
+              <a href={`/infoBikes/${stations.id}`}>
                 <Button p={10} color="green.900" >
                   Consultar
                 </Button>
-                </a>
-              </Flex>
-              {stations.id} <br />
-              {stations.name} <br />
-              
-            </Box>
-            
-            ))}
-        
+              </a>
+            </Flex>
+            {stations.id} <br />
+            {stations.name} <br />
+
+          </Box>
+
+        ))}
+
 
       </section>
-      
+
 
     </>
   )
--- src/InfoBikes.jsx
+++ src/InfoBikes.jsx
@@ -1,43 +1,108 @@
 import { useState, useEffect } from "react";
-import { Load } from "./services/stations";
+import { GetStationById } from "./services/stations";
 import imagenLogo from "./assets/planeta.png";
-import { Heading, Box, Image,Flex, Center, Text, Spacer, Tag, Button} from '@chakra-ui/react';
+import { useParams } from "react-router-dom";
+import { Heading, Box, Image, Flex, Center, Text, Spacer, Tag, Button } from '@chakra-ui/react';
 
-export default function InfoBikes() {
-  const [stations, setStations] = useState([]);
+let countB = 0;
+let countEstaciones = 0;
+let countE = 0;
+
+const Network = () => {
+    const [network, setNetwork] = useState(null);
+
+    const params = useParams();
+    useEffect(() => {
+        GetStationById(params.id, setNetwork);
+    }, []);
+
+    if (network != null) {
+        network.stations.map((station) => {
+            countB += station.free_bikes;
+            countE += station.empty_slots;
+            countEstaciones++;
+
+        });
+    }
+    let estaciones = countEstaciones / 4;
+    let espacios = countE / 4;
+    let bikes = countB / 4;
+
+    return (
+        <>
+            <center>
+                <Heading as="h1" size="lg">
+                    <Box width={400} background="orange.200" borderRadius={20} p={10} m={5}>
+                        {params.id}
+                    </Box>
+                </Heading>
+                <a href="/">
+                    <Image src={imagenLogo} width={300} m={4}></Image>
+                </a>
+
+                <Box width={600} background="yellow.100" borderRadius={20}>
+                    <Text fontSize="xl" fontFamily="monospace" p={4} align="center">
+                        <p><strong> Estaciones totales </strong><br />
+                            {estaciones}</p>
+                        <p> <strong>  Bicicletas totales </strong><br />
+                            {bikes}</p>
+                        <p><strong> Espacios totales </strong><br />
+                            {espacios}</p>
+                    </Text>
+                </Box>
+            </center>
+            {network != null
+                ? network.stations.map((station) => (
+
+                    <Box key={station.name} bg="yellow.100" p={5} m={5} borderRadius={20} textAlign="center">
+                        <strong><Text fontSize={25} m={5}>{station.name}</Text></strong>
 
 
-  useEffect(() => {
-    Load(setStations);
-  }, []);
 
-  return (
-    <>
-    
-    
-   <Center> <Image m={4} src={imagenLogo} alt="BikeLogo" width={200}/> </Center> 
-      <Heading as="h1" size="4xl">Bikesssssssssssssss</Heading>
+                        {station.free_bikes || station.empty_slots != null ? (
+                            <Box>
+                                <Flex>
+                                    <Spacer />
+                                    <Center w="300px" bg="orange.200" borderRadius={5}>
+                                        <Text>Bicicletas libres <br />
+                                            <center>{station.free_bikes}</center></Text>
+                                    </Center>
+                                    <Spacer />
+                                    <Center w="300px" bg="orange.200" borderRadius={5}>
+                                        <Text>Espacios libres <br />
+                                            <center>{station.empty_slots}</center></Text>
+                                    </Center>
+                                    <Spacer />
+                                    <Center w="300px" bg="orange.200" borderRadius={5}>
+                                        <Text> Total de espacios{" "}<br />
+                                            <center>{station.free_bikes + station.empty_slots}</center></Text>
+                                    </Center>
+                                    <Spacer />
+                                    <br />
+                                </Flex>
+                                <strong><Text fontSize={10} color="grey">{station.timestamp}</Text></strong>
+                            </Box>
+                        ) : (
+                            <Flex>
 
-      <section>
-        
-          {stations.map((stations) => (
+                                <Center w="300px" bg="red.200" borderRadius={5}>
+                                    <Text> No está disponible actualmente</Text>
+                                </Center>
 
-            <Box key={stations.id} bg="yellow.100" p={10} m={5} borderRadius={20}>
-              
-                
-              {stations.id} <br />
-              
-              
-            </Box>
-            
-            ))}
-        
+                                <Spacer />
+                                <Tag p={2} colorScheme="red">
+                                    {station.timestamp}
+                                </Tag>
+                            </Flex>
+                        )}
+                    </Box>
+                ))
+                : <center><Box width={400} background="orange.200" borderRadius={20} p={10} m={5} textAlign="center">
+                    Cargando estaciones ... <br />
+                    Por favor espere un momento
+                </Box></center>}
+        </>
+    );
+};
 
-      </section>
-      
-
-    </>
-  )
-
-}
-
+export default Network;
